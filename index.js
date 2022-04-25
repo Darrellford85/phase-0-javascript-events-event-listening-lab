cab4246174bf86ce0a8aa4acc9cac4542d7a96bf
Darrell Ford <Darrellford85@gmail.com>
2022-04-25 09:52:35 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+//Working these addevents //
+
+function addingEventListener(){
+    const input = document.getElementById('input');
+    return input.addEventListener('click', function() {
+        alert('Ouch, I was clicked!!!');
+
+    });
 }
